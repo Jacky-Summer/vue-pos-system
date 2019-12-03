49d302535f25ef12a088e736e725056ce938424f
Jacky-Summer <1106619871@qq.com>
2019-12-03 22:59:28 +0800

    init vue-pos project

---
+++ src/components/Pos.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="pos">
+  
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Pos',
+  data () {
+    return {
+
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
