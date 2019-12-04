d23a33a5415d99b96168b70a474bed0642527689
Jacky-Summer <1106619871@qq.com>
2019-12-04 21:22:58 +0800

    add left navbar

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,32 @@
 <template>
   <div id="app">
-    <router-view/>
+    <!--左侧导航-->
+    
+      <leftNav></leftNav>
+    
+    <!--操作区域-->
+    <div class="main">
+      <router-view></router-view>
+    </div>
   </div>
 </template>
 
 <script>
+import leftNav from '@/components/common/leftNav'
 export default {
-  name: 'App'
+  name: 'App',
+  components: {
+    leftNav
+  }
 }
 </script>
 
 <style>
+.main{
+  float:left;
+  width:95%; 
+  background-color: #EFF2F7;
+  height:100%;
+  overflow: auto;
+}
 </style>
---
+++ src/components/common/leftNav.vue
@@ -0,0 +1,60 @@
+<template>
+  <div class="left-nav">
+    <ul>
+        <li>
+            <span class="icon iconfont">&#xe601;</span>
+            <div>收银</div>
+        </li>
+        <li>
+            <span class="icon iconfont">&#xe602;</span>
+            <div>店铺</div>
+        </li>
+        <li>
+            <span class="icon iconfont">&#xe600;</span>
+            <div>商品</div>
+        </li>
+        <li>
+            <span class="icon iconfont">&#xe603;</span>
+            <div>会员</div>
+        </li>
+        <li>
+            <span class="icon iconfont">&#xe606;</span>
+            <div>统计</div>
+        </li>
+    </ul>
+  </div>
+</template>
+ 
+<script>
+export default {
+  name: 'leftNav',
+  data () {
+    return {
+    }
+  }
+}
+</script>
+<style>
+.left-nav{
+    color:#fff;
+    font-size:10px;
+    height:100%;
+    background-color: #1D8ce0;
+    float:left;
+    width:5%;
+}
+.iconfont{
+    font-size:24px;
+}
+.left-nav ul{
+    padding:0px;
+    margin: 0px;
+}
+.left-nav li{
+    list-style: none;
+    text-align: center;
+    border-bottom:1px solid #20a0ff;
+    padding:10px;
+    cursor: pointer;
+}
+</style>
